3584e30217f731094f2e1bd434d9b0f05cfc46bc
Danil Kavaliou <dkavaliou.by@gmail.com>
2025-09-06 18:26:02 +0400

    002_03_Local_strategy

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import type { Response } from 'express';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './users/current-user.decorator';
+import { UserDocument } from './users/models/user.schema';
+import { LocalAuthGuard } from './guards/local-auth.guards';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response,
+  ){
+    await this.authService.login(user, response);
+    response.send(user);
   }
 }
---
+++ apps/auth/src/strategies/local.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-local";
+import { UsersService } from "../users/users.service";
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly usersService: UsersService, // Make sure UsersService is properly imported and injected
+  ) {
+    super({
+      usernameField: "email",
+    });
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    try {
+      const user = await this.usersService.verifyUser(email, password);
+      return user;
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
+  }
+
+}
